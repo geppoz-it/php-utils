aa39ab0234370d6a3e81c6933b2065e80a7838a3
geppoz-it <gico72@gmail.com>
2016-11-24 17:20:33 +0100

    Create utils.php

---
+++ utils.php
@@ -0,0 +1,14 @@
+function dumpToTable($array){
+        if (!is_array($array)){
+                return htmlspecialchars($array);
+        } else {
+                $txt="<table style='width:100%;border-collapse:collapse;font-size:9pt;font-family:arial;'>";
+                foreach ($array as $k=>$v){
+                        $txt.="<tr><td style='border-style:solid;border-width:1px;'>$k</td><td style='width:100%;border-style:solid;border-width:1px;'>";
+                        $txt.=dumpToTable($v);
+                        $txt.="</td></tr>";
+                }
+                $txt.="</table>";
+                return $txt;
+        }
+}
