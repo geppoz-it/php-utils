50ae8ed833fbe327bac5b4ac4402822ecc6aee85
geppoz-it <gico72@gmail.com>
2016-11-24 17:27:24 +0100

    Update utils.php

--- utils.php
+++ utils.php
@@ -5,7 +5,7 @@ function dump_to_table($array){
                 $txt="<table style='width:100%;border-collapse:collapse;font-size:9pt;font-family:arial;'>";
                 foreach ($array as $k=>$v){
                         $txt.="<tr><td style='border-style:solid;border-width:1px;'>$k</td><td style='width:100%;border-style:solid;border-width:1px;'>";
-                        $txt.=dumpToTable($v);
+                        $txt.=dump_to_table($v);
                         $txt.="</td></tr>";
                 }
                 $txt.="</table>";
